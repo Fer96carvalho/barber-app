24957b205c9254c0e3e2cdcf7e03013fbeeab28a
Fer96carvalho <f.luiz1996@gmail.com>
2024-01-30 02:20:22 -0300

    Add welcome mensage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,20 @@
+import { ptBR } from "date-fns/locale/pt-BR";
 import Header from "../_components/header";
+import { format, formatDate } from "date-fns";
 
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Fernando!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEE ',' dd 'de' MMMM", {
+          locale: ptBR
+          })}
+        </p>
+      </div>
     </div>
   );
 }
