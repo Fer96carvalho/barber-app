3fd93ef346db123db06c50b930618b1b0f54f6e6
Fer96carvalho <f.luiz1996@gmail.com>
2024-01-30 13:54:40 -0300

    add initial booking component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'https://utfs.io/**'
+                
+            }
+        ]
+    }
+};
 
 export default nextConfig;
